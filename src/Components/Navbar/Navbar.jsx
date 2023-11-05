50a919ec17bc9b78905306cc194fabf39afe9b81
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-11-06 00:31:32 +0600

    navbar logo added and a few changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
     return (
-        <div>
+        <div className=" w-10/12 mx-auto">
             <div className="navbar ">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -10,32 +10,33 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+                            <li><a>Home</a></li>
                             <li>
-                                <a>Parent</a>
+                                <a>Services</a>
                                 <ul className="p-2">
                                     <li><a>Submenu 1</a></li>
                                     <li><a>Submenu 2</a></li>
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li><a>Login</a></li>
                         </ul>
                     </div>
+                    <img className="mr-2 h-[35px]" src="/logo.svg" alt="" />
                     <a className="btn btn-ghost normal-case text-xl">ToolShare</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
+                        <li><a>Home</a></li>
                         <li tabIndex={0}>
                             <details>
-                                <summary>Parent</summary>
+                                <summary>Services</summary>
                                 <ul className="p-2">
                                     <li><a>Submenu 1</a></li>
                                     <li><a>Submenu 2</a></li>
                                 </ul>
                             </details>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Login</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
